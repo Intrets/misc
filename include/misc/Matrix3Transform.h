8fec624ad865fa9553f9808a2e8861f66be80c82
Intrets <geen.inspiratie824@gmail.com>
2022-02-25 03:51:32 +0100

    more mat3 transformation functions

--- include/misc/Matrix3Transform.h
+++ include/misc/Matrix3Transform.h
@@ -23,6 +23,30 @@
 
 namespace misc::mat3
 {
+	inline glm::mat3 from_mat4(glm::mat4 const& m) {
+		return glm::mat3(
+			m[0][0], m[0][1], m[0][3],
+			m[1][0], m[1][1], m[1][3],
+			m[3][0], m[3][1], m[3][3]
+		);
+	}
+
+	inline glm::mat3 scale(float s) {
+		return glm::mat3(
+			s, 0.0f, 0.0f,
+			0.0f, s, 0.0f,
+			0.0f, 0.0f, 1.0f
+		);
+	}
+
+	inline glm::mat3 scale(glm::vec2 s) {
+		return glm::mat3(
+			s.x, 0.0f, 0.0f,
+			0.0f, s.y, 0.0f,
+			0.0f, 0.0f, 1.0f
+		);
+	}
+
 	inline glm::mat3 rotate(float a) {
 		float const s = std::sin(a);
 		float const c = std::cos(a);
