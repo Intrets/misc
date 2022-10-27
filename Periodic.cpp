b59db2acecd4f7c0288ee00dde18b9a4fe680f5f
Intrets <geen.inspiratie824@gmail.com>
2022-10-28 00:52:32 +0200

    Periods and Intervals timing

---
+++ Periodic.cpp
@@ -0,0 +1,58 @@
+#include "misc/Periodic.h"
+
+#include <cassert>
+
+namespace misc
+{
+	Interval::Interval(milliseconds_type period_) : period(period_) {
+	}
+
+	bool Interval::run() {
+		auto now = std::chrono::steady_clock::now();
+		auto triggerTime = this->last + std::chrono::nanoseconds(static_cast<int64_t>(this->period * 1'000'000));
+		if (now > triggerTime) {
+			this->last = now;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	int64_t Interval::countRuns() {
+		auto now = std::chrono::steady_clock::now();
+
+		if (this->last == std::chrono::steady_clock::time_point{}) {
+			this->last = now;
+			return 1;
+		}
+
+		auto p = std::chrono::nanoseconds(static_cast<int64_t>(this->period * 1'000'000));
+
+		auto diff = now - this->last;
+		auto count = std::max((long long)0, diff / p);
+
+		this->last += count * p;
+
+		return static_cast<int64_t>(count);
+	}
+
+	void Interval::reset() {
+		this->last = {};
+	}
+
+	int Periods::run() {
+		this->period = (this->period + this->countRuns()) % this->periodCount;
+		assert(this->period >= 0);
+
+		return this->period;
+	}
+
+	void Periods::reset() {
+		this->Interval::reset();
+		this->period = -1;
+	}
+
+	Periods::Periods(milliseconds_type period, int periodCount_) : Interval(period), periodCount(periodCount_) {
+	}
+}
---
+++ include/misc/Periodic.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <chrono>
+
+namespace misc
+{
+	struct Interval
+	{
+		using milliseconds_type = float;
+
+		static constexpr milliseconds_type seconds(auto v) {
+			return static_cast<milliseconds_type>(v) * 1000;
+		}
+
+		static constexpr milliseconds_type milliseconds(auto v) {
+			return static_cast<milliseconds_type>(v);
+		}
+
+		milliseconds_type period;
+		std::chrono::steady_clock::time_point last{};
+
+		Interval() = delete;
+		Interval(milliseconds_type period_);
+		~Interval() = default;
+
+		bool run();
+		int64_t countRuns();
+		void reset();
+	};
+
+	struct Periods : Interval
+	{
+		int periodCount = 0;
+		int period = -1;
+
+		int run();
+		void reset();
+
+		Periods() = delete;
+		Periods(milliseconds_type period, int periodCount_);
+		~Periods() = default;
+	};
+}
