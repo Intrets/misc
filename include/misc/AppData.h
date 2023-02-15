3c5bdba4b744b7b2fa3b46a70e06620495e9f495
Intrets <geen.inspiratie824@gmail.com>
2023-02-15 01:51:43 +0100

    sub paths for getAppData and a file variant

--- include/misc/AppData.h
+++ include/misc/AppData.h
@@ -5,7 +5,7 @@
 
 namespace misc
 {
-	inline std::optional<std::filesystem::path> getAppDataFolder(std::filesystem::path subPath) {
+	inline std::optional<std::filesystem::path> getAppDataFolder(std::initializer_list<std::filesystem::path> subPaths) {
 		char* buffer = nullptr;
 		size_t size;
 		auto err = _dupenv_s(&buffer, &size, "AppData");
@@ -20,10 +20,23 @@ namespace misc
 		std::string pathString(buffer, size - 1);
 		free(buffer);
 
-		std::filesystem::path path = pathString / subPath;
+		std::filesystem::path path = pathString;
+
+		for (auto& subPath : subPaths) {
+			path /= subPath;
+		}
 
 		std::filesystem::create_directories(path);
 
 		return path;
+	}
+
+	inline std::optional<std::filesystem::path> getAppDataFile(std::initializer_list<std::filesystem::path> subPaths, std::filesystem::path file) {
+		if (auto path = getAppDataFolder(subPaths)) {
+			return path.value() / file;
+		}
+		else {
+			return std::nullopt;
+		}
 	}
 }
